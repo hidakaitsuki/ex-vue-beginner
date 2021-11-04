ad2623ebb2257136776e5f9135bd8b1ad4ad4443
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-11-04 15:38:22 +0900

    Ex04完了

--- src/views/answer/ex04.vue
+++ src/views/answer/ex04.vue
@@ -1,16 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <input type="text" v-model.number="num1" /><br />
+    <input type="text" v-model.number="num2" /><br />
+    <input type="text" v-model.number="num3" /><br />
+    税抜き価格：{{ answer }}円 <br />
+    税込み価格：{{ taxAnswer }}円
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 @Component
 export default class ex04 extends Vue {
-
+  num1 = 0;
+  num2 = 0;
+  num3 = 0;
+  readonly tax = 1.1;
+  get answer() {
+    return Number(this.num1 + this.num2 + this.num3).toLocaleString();
+  }
+  get taxAnswer() {
+    return Number(
+      this.num1 + this.num2 + this.num3 * this.tax
+    ).toLocaleString();
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
